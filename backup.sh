764136e3d1aaa3488a588594c81be805232c4f9e
Yangfan Zhang <yangfanzhang@gmail.com>
2015-01-29 21:16:23 +0000

    remove backup file locally to save space

--- backup.sh
+++ backup.sh
@@ -4,5 +4,5 @@ filename=arrival-$(date +"%Y-%m-%d-%H:%M:%S").sql
 
 mysqldump delay > /home/delay/db/$filename
 scp /home/delay/db/$filename yz10111@shell1.doc.ic.ac.uk:/vol/bitbucket/yz10111/delaybackup
-
+rm /home/delay/db/$filename
 
