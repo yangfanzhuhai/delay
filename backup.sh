a558ae4e6e5557bdd0963279739a0301f5008dde
Yangfan Zhang <yangfanzhang@gmail.com>
2015-02-12 12:06:39 +0000

    updated backup destination

--- backup.sh
+++ backup.sh
@@ -3,6 +3,6 @@
 filename=arrival-$(date +"%Y-%m-%d-%H:%M:%S").sql
 
 mysqldump delay > /home/delay/db/$filename
-scp /home/delay/db/$filename yz10111@shell1.doc.ic.ac.uk:/vol/bitbucket/yz10111/delaybackup
+scp /home/delay/db/$filename yz10111@shell1.doc.ic.ac.uk:/vol/automed/data/tfl_bus/
 rm /home/delay/db/$filename
 
