5ba9b95ef4942455d6ff5b6d9126b2ae14d157eb
Yangfan Zhang <yangfanzhuhai@gmail.com>
2015-03-12 16:38:25 +0000

    with start_stop and end_stop in time_table

--- sql/average_by_hour.sql
+++ sql/average_by_hour.sql
@@ -4,5 +4,5 @@ CREATE TABLE time_table (
     SELECT day, hour, TRUNCATE(AVG(travel_time), 1) average_travel_time
     FROM travel_time_log_main
     WHERE travel_time > 0
-    GROUP BY day, hour
+    GROUP BY start_stop, end_stop, day, hour
 ); 
