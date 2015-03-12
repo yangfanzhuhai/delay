7248972c6b25221be2c89b02b344519e423c55f6
Yangfan Zhang <yangfanzhuhai@gmail.com>
2015-03-12 16:35:18 +0000

    fixed bug

--- sql/average_by_hour.sql
+++ sql/average_by_hour.sql
@@ -1,7 +1,7 @@
 USE delay; 
 
 CREATE TABLE time_table (
-    SELECT day, HOUR(start_time) hour, TRUNCATE(AVG(travel_time), 1) average_travel_time
+    SELECT day, hour, TRUNCATE(AVG(travel_time), 1) average_travel_time
     FROM travel_time_log_main
     WHERE travel_time > 0
     GROUP BY day, hour
