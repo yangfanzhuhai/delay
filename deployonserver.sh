998176dd08d7fd8ca012707ef00f6348279cede1
Yangfan Zhang <yangfanzhuhai@gmail.com>
2015-04-30 16:09:15 +0100

    updated deploy script

--- deployonserver.sh
+++ deployonserver.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 source /home/delay/.virtualenvs/delay/bin/activate     # Activate the virtualenv
+cd /home/delay/delay/
 git pull
 pip install -r requirements.txt           # Install or upgrade dependencies
-python delaysite/manage.py migrate                  # Apply South's database migrations
-python delaysite/manage.py compilemessages          # Create translation files
-python delaysite/manage.py collectstatic --noinput  # Collect static files
+cd /home/delay/delay/delaysite
+python manage.py migrate                  # Apply South's database migrations
+python manage.py compilemessages          # Create translation files
+python manage.py collectstatic --noinput  # Collect static files
