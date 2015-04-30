5ac4e875f900756f84892414427226ef5a522e3c
Yangfan Zhang <yangfanzhuhai@gmail.com>
2015-04-30 16:06:47 +0100

    updated requirements.txt

--- deployonserver.sh
+++ deployonserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /home/delay/.virtualenvs/delay/bin/activate     # Activate the virtualenv
 git pull
-pip install -r delaysite/requirements.txt           # Install or upgrade dependencies
+pip install -r requirements.txt           # Install or upgrade dependencies
 python delaysite/manage.py migrate                  # Apply South's database migrations
 python delaysite/manage.py compilemessages          # Create translation files
 python delaysite/manage.py collectstatic --noinput  # Collect static files
