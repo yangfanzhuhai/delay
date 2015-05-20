828dfa7739c255aca0438a1554bc47bebf14f3d2
Yangfan Zhang <yangfanzhuhai@gmail.com>
2015-05-20 13:18:46 +0100

    updated arrivald script

--- arrivald.py
+++ arrivald.py
@@ -6,8 +6,7 @@ import sys
 from datetime import datetime
 
 # url = ('http://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1'
-url = ('http://countdown.api.tfl.gov.uk/interfaces/ura/stream_V1'
-       '?ReturnList=StopID,LineName,VehicleID,TripID,EstimatedTime,ExpireTime')
+url = ('http://countdown.api.tfl.gov.uk/interfaces/ura/stream_V1?ReturnList=StopID,LineName,DirectionID,VehicleID,TripID,EstimatedTime,ExpireTime')
 
 r = requests.get(
     url,
@@ -33,36 +32,53 @@ conn = pymysql.connect(
 )
 cur = conn.cursor()
 
-sql_insert = ("INSERT INTO delay_arrivals "
-              "(stop_code_lbsl, route, vehicle_id, trip_id, "
-              "arrival_time, expire_time, arrival_date) "
-              "VALUES (%s, %s, %s, %s, %s, %s, DATE(%s)) "
-              "ON DUPLICATE KEY UPDATE "
-              "arrival_time = CASE "
-              "WHEN TIMESTAMPDIFF(MINUTE, recorded_time, now()) < 10 "
-              "THEN VALUES(arrival_time) ELSE arrival_time END, "
-              "expire_time = CASE "
-              "WHEN TIMESTAMPDIFF(MINUTE, recorded_time, now()) < 10 "
-              "THEN VALUES(expire_time) ELSE expire_time END, "
-              "recorded_time = CASE "
-              "WHEN TIMESTAMPDIFF(MINUTE, recorded_time, now()) < 10 "
-              "THEN now() ELSE recorded_time END; ")
+sql_insert_full = ("INSERT INTO delay_arrivals_full "
+                   "(stop_code_lbsl, route, run, vehicle_id, trip_id, "
+                   "arrival_time, expire_time, arrival_date) "
+                   "VALUES (%s, %s, %s, %s, %s, %s, %s, DATE(%s)) ")
 
+sql_insert = ("INSERT INTO delay_arrivals "
+              "(stop_code_lbsl, route, run, vehicle_id, trip_id, "
+              "arrival_time, expire_time, arrival_date) "
+              "VALUES (%s, %s, %s, %s, %s, %s, %s, DATE(%s)) ")
+
+sql_update = ("UPDATE delay_arrivals "
+              "SET arrival_time = %s, expire_time = %s "
+              "WHERE id = %s")
+
+sql = ("SELECT id FROM delay_arrivals "
+       "WHERE stop_code_lbsl = %s "
+       "AND route = %s AND run = %s AND vehicle_id = %s "
+       "AND trip_id = %s "
+       "AND TIMESTAMPDIFF(MINUTE, recorded_time, now()) < 10 ")
 
 for line in r.iter_lines():
     if not line:
         continue
-    # print(line)
     line = json.loads(line.decode('utf-8-sig'))
     if line[0] != 1:
         continue
-    # print(line)
     line = line[1:]
-    for i in [4, 5]:
+    for i in [5, 6]:
         line[i] = int(line[i] / 1000)
-        line[i] = datetime.fromtimestamp(line[i])
+    line[5] = datetime.fromtimestamp(line[5])
+    if line[6] == 0:
+        line[6] = None
+    else:
+        line[6] = datetime.fromtimestamp(line[6])
     line.append(line[5])
-    # print(line)
-    cur.execute(sql_insert, line)
+    print(line[0:5], line[5].strftime("%H:%M:%S"), line[6])
+    cur.execute(sql_insert_full, line)
+    cur.execute(sql, line[0:5])
+    re = cur.fetchone()
+    if re is None:
+        cur.execute(sql_insert, line)
+    else:
+        print(re)
+        lineupdate = line[5:7]
+        # lineupdate.extend(line[0:5])
+        lineupdate.extend(re)
+        # print (lineupdate)
+        cur.execute(sql_update, lineupdate)
     conn.commit()
 conn.close()
