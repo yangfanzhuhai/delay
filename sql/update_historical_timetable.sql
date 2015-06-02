4e190979694b5a300b65fe847d981d8d4e55bf0b
Yangfan Zhang <yangfanzhuhai@gmail.com>
2015-06-02 12:13:06 +0100

    added update historical script

---
+++ sql/update_historical_timetable.sql
@@ -0,0 +1,13 @@
+TRUNCATE delay_timetable_updated;
+
+INSERT INTO delay_timetable_updated (start_stop, end_stop, day, hour, average_travel_time)
+(
+  SELECT start_stop, end_stop, day, hour, AVG(average_travel_time) AS average_travel_time
+  FROM delay_current_timetable_log
+  GROUP BY start_stop, end_stop, day, hour
+);
+
+INSERT INTO delay_timetable (start_stop, end_stop, day, hour, average_travel_time)
+SELECT start_stop, end_stop, day, hour, average_travel_time
+FROM delay_timetable_updated
+ON DUPLICATE KEY UPDATE delay_timetable.average_travel_time = delay_timetable_updated.average_travel_time;
