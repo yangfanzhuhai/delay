525fbfd6438606a4d8131b8d0cfdf5e1703937be
Yangfan Zhang <yangfanzhuhai@gmail.com>
2015-06-03 19:35:31 +0100

    updated historical timetable no lock

--- sql/update_historical_timetable.sql
+++ sql/update_historical_timetable.sql
@@ -3,6 +3,7 @@ USE delay;
 
 TRUNCATE delay_timetable_updated;
 
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 INSERT INTO delay_timetable_updated (start_stop, end_stop, day, hour, average_travel_time)
 (
   SELECT start_stop, end_stop, day, hour, AVG(average_travel_time) AS average_travel_time
@@ -14,3 +15,4 @@ INSERT INTO delay_timetable (start_stop, end_stop, day, hour, average_travel_tim
 SELECT start_stop, end_stop, day, hour, average_travel_time
 FROM delay_timetable_updated
 ON DUPLICATE KEY UPDATE delay_timetable.average_travel_time = delay_timetable_updated.average_travel_time;
+COMMIT;
